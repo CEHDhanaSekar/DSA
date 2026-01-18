b7b0b91b921c0c1c90586d66fb0270cadc1b015d
Dhanasekar R <viratvikii1618@gmail.com>
2026-01-18 19:24:30 +0530

    Add BinarySearch and LinearSearch algorithms
    Implemented BinarySearch and LinearSearch classes with static methods for searching and checking existence in arrays, strings, and 2D arrays. BinarySearch supports both ascending and descending sorted arrays. Also updated the project file to include a folder for future sorting algorithms.

---
+++ Algorithms/Searching/BinarySearch.cs
@@ -0,0 +1,68 @@
+﻿namespace DSA.Algorithms.Searching;
+
+public class BinarySearch
+{
+    public static int Search(int[] arr, int target)
+    {
+        int start = 0;
+        int end = arr.Length - 1;
+
+        bool isAsc = arr[start] < arr[end];
+
+        while(start <= end)
+        {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target)
+                return mid;
+
+            if(isAsc)
+            {
+                if (arr[mid] < target)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+            else
+            {
+                if (arr[mid] > target)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }        
+        }
+
+        return -1;
+    }
+
+    public static bool Contains(int[] arr, int target)
+    {
+        int start = 0;
+        int end = arr.Length - 1;
+
+        bool isAsc = arr[start] < arr[end];
+
+        while (start <= end)
+        {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target)
+                return true;
+
+            if (isAsc)
+            {
+                if (arr[mid] < target)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+            else
+            {
+                if (arr[mid] > target)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Algorithms/Searching/LinearSearch.cs
@@ -0,0 +1,80 @@
+﻿namespace DSA.Algorithms.Searching;
+
+public class LinearSearch
+{
+    public static int Search(int[] arr, int target)
+    {
+        for (int i = 0; i < arr.Length; i++)
+            if (arr[i] == target) return i;
+
+        return -1;
+    }
+
+    public static bool Contains(int[] arr, int target)
+    {
+        for (int i = 0; i < arr.Length; i++)
+            if (arr[i] == target) return true;
+
+        return false;
+    }
+
+    public static int Search(string str, char target)
+    {
+        for (int i = 0; i < str.Length; i++)
+            if (str[i] == target) return i;
+
+        return -1;
+    }
+
+    public static bool Contains(string str, char target)
+    {
+        for (int i = 0; i < str.Length; i++)
+            if (str[i] == target) return true;
+
+        return false;
+    }
+
+    public static int Search(string[] strArr, string target)
+    {
+        for (int i = 0; i < strArr.Length; i++)
+            if (strArr[i] == target) return i;
+
+        return -1;
+    }
+
+    public static bool Contains(string[] strArr, string target)
+    {
+        for (int i = 0; i < strArr.Length; i++)
+            if (strArr[i] == target) return true;
+
+        return false;
+    }
+
+
+    // 2D Array Search
+    public static int[] Search(int[][] arr, int target)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = 0; j < arr[i].Length; i++)
+            {
+                if (arr[i][j] == target) return [i, j];
+            }
+        }
+
+        return [-1, -1];
+    }
+
+    public static int[] Search(string[][] arr, string target)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = 0; j < arr[i].Length; i++)
+            {
+                if (arr[i][j] == target) return [i, j];
+            }
+        }
+
+        return [-1, -1];
+    }
+}
